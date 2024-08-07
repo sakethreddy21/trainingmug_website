2ef60604ed53ddf2e3e33b70eac683e0197ee2ee
saketh <saketh.reddy@trainingmug.com>
2024-08-07 19:59:04 +0530

    little changes

--- components/second-screen/page.tsx
+++ components/second-screen/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from '../ui/moving-border'
+import { ArrowRight } from 'lucide-react'
 
 const SecondScreen = () => {
   return (
     <div className='flex flex-col justify-center items-center  py-32'>
-         <button className="px-4 py-2 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-        Simple
+         <button className="flex flex-row justify-between items-center px-4 py-2 rounded-md border border-neutral-300 bg-[#39c3ef] text-black font-medium text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+        See Courses
+        <ArrowRight className='ml-2' size={15} />
       </button>
       <div className='py-32'>
       <Button
