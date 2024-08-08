2bd50f0993097127d9468dfdee2089288b9a8c9f
saketh <saketh.reddy@trainingmug.com>
2024-08-08 16:12:50 +0530

    tabs are added

--- components/second-screen/page.tsx
+++ components/second-screen/page.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Button } from '../ui/moving-border'
 import { ArrowRight } from 'lucide-react'
+import { TabsDemo } from '../tabs/page'
 
 const SecondScreen = () => {
   return (
-    <div className='flex flex-col justify-center items-center  py-32'>
+    <div className='flex flex-col justify-center items-center  py-10'>
          <button className="flex flex-row justify-between items-center px-4 py-2 rounded-md border border-neutral-300 bg-[#39c3ef] text-black font-medium text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
         See Courses
         <ArrowRight className='ml-2' size={15} />
       </button>
-      <div className='py-32'>
+      <div className='py-10'>
       <Button
         borderRadius="1.75rem"
         duration={10000}
@@ -17,6 +18,9 @@ const SecondScreen = () => {
       >
      <iframe className='w-full h-full' src='https://www.youtube.com/embed/5-MLMzbUlPY' title='How to become full stack developer roadmap' allowFullScreen></iframe>
       </Button>
+      </div>
+      <div className='w-full h-screen'>
+        <TabsDemo />
       </div>
       
       
