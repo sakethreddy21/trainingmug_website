15a2eab657560e8fe3c044dab0475275540da790
saketh <saketh.reddy@trainingmug.com>
2024-08-08 16:38:08 +0530

    hero component updated

--- components/description/page.tsx
+++ components/description/page.tsx
@@ -3,7 +3,7 @@ import { FlipWords } from '@/components/ui/flip-words';
 
 const Description = () => {
 
-    const words = ["Full stack developer", "Data Scientist", "Cloud Engineer", "App Developer"];
+    const words = ["Full Stack Java Developer ", "Junior Software Engineer", "Frontend Developer", "Backend Developer"];
   return (
     <div className="flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-[36px] text-neutral-600 dark:text-neutral-400">
